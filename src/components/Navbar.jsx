2fb855a8ac37f592d4ca5bdc76db10771fb9e23f
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-06-11 15:34:36 +0530

    add: newup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,13 @@ import {
   MDBNavbarItem,
   MDBNavbarLink,
   MDBIcon,
-  MDBCollapse
+  MDBCollapse,
+  MDBCol,
+  MDBRow,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownItem,
+  MDBDropdownMenu
 } from 'mdb-react-ui-kit';
 
 export default function Navbar() {
@@ -16,9 +22,20 @@ export default function Navbar() {
   return (
     <>
       <MDBCollapse open={openNavExternal}>
-        <div className='bg-hsl(218, 41%, 15%) p-4'>
-          <h5 className='text-white h4'>Collapsed content</h5>
-          <span className='text-muted'>Toggleable via the navbar brand.</span>
+        <div className='d-flex p-2 gap-4'>
+        <MDBIcon fas icon="user-circle" size='2x' /> 
+        <p className='text-muted mt-1'><MDBIcon fas icon="envelope"/> [user@gmail.com]</p>
+          <MDBDropdown>
+                <MDBDropdownToggle tag='a' className='nav-link  p-0 hidden-arrow' role='button'  >
+                <MDBIcon fas icon="cog" className='mt-2' size='1x' />
+                </MDBDropdownToggle>
+                <MDBDropdownMenu style={{backgroundColor:'hsl(218, 31%, 9%)'}}>
+                  <MDBDropdownItem link ><MDBIcon fas icon="user-circle" /> Profile</MDBDropdownItem>
+                  <MDBDropdownItem link ><MDBIcon fas icon="cogs" /> Settings</MDBDropdownItem>
+                  <MDBDropdownItem link ><MDBIcon fas icon="user-plus" /> Add new account</MDBDropdownItem>
+                  <MDBDropdownItem link href='/'  ><MDBIcon fas icon="sign-out-alt" /> Logout</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
         </div>
       </MDBCollapse>
       <MDBNavbar dark bgColor='hsl(218, 41%, 15%)'>
@@ -34,7 +51,7 @@ export default function Navbar() {
             <MDBIcon icon='bars' fas />
           </MDBNavbarToggler>
           <MDBNavbarBrand className='p-0'>
-          <h1 className='text-dark text-bold font-monospace'>SHUBOOK</h1>
+          <h1 className='text-bold font-monospace' style={{color:'hsl(218, 51%, 45%)'}}>SHUBOOK</h1>
           </MDBNavbarBrand>
         </MDBContainer>
       </MDBNavbar>
