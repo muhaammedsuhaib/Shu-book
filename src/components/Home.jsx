a306895114fedaf542acc781d77f6d44e199a2b7
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-06-26 20:55:09 +0530

    add:newww

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,13 @@ import { useNavigate } from 'react-router-dom';
 import { passing } from './Main';
 
 const Home = () => {
-  const {userData,setUserData} = useContext(passing);
+  const { userData, setUserData } = useContext(passing);
   const nav = useNavigate();
-  
+
   return (
-    <div className="container-fluid w-100 p-0" style={{ height: '100vh', overflow: 'hidden' }}>
-      {/* <h1 className='text-start text-primary p-2'>SHUBOOK</h1> */}
+    <div className="container-fluid w-100 p-0 position-relative" style={{ height: '100vh', overflow: 'hidden'}}>
+      <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+    
       <img src="/Logo.png" alt="" className="bg-transparent zoom-image" width={"200px"} />
       <div className="container p-5" style={{ display: 'grid', placeItems: 'center' }}>
         <h1 className='text-center text-white display-2'>Organize your work and life, finally.</h1>
@@ -19,9 +20,17 @@ const Home = () => {
           Welcome to SHUBOOK, where productivity meets simplicity – your ultimate destination to organize tasks, streamline schedules, and conquer your day with ease.
         </p>
         <div className="container text-center">
-          <MDBBtn variant="primary" rounded className="btn btn-lg btn-primary text-center" onClick={() => nav(!userData ? '/login' : '/blog')}>
-            <img src="/button-logo.png" alt="Logo" style={{ width: '34px' }} />
-            Get Started
+          <MDBBtn
+            variant="primary"
+            rounded
+            className="btn btn-lg btn-primary text-center"
+            style={{
+              background: 'radial-gradient(circle, #44006b, #ad1fff)'        
+            }}
+            onClick={() => nav(!userData ? '/login' : '/blog')}
+          >
+            <img src="/button-logo.png" alt="Logo" style={{ width: '34px', marginRight: '8px' }} className='zoom-btn' />
+            <span className='zoom-btn'>Get Started</span>
           </MDBBtn>
         </div>
       </div>
