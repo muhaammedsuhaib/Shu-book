19b918256b859f9a6cead0fb3e00d572d9286185
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-11-25 21:15:36 +0530

    feat:"add user dashbord"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+    
   return (
     <div className="min-h-screen bg-black text-white flex flex-col justify-center items-center">
       <div className="text-center max-w-3xl mx-auto px-4">
@@ -11,7 +15,7 @@ const Home = () => {
         <p className="mt-4 text-lg text-gray-400">
           Your ultimate task management solution for productivity and success.
         </p>
-        <Link to={isAuthenticated ? "page" : "/login"}>
+        <Link to={isAuthenticated?"page":"/login"}>
           <button className="mt-6 bg-white text-black px-6 py-3 rounded-lg shadow-md hover:bg-gray-300 transition duration-300">
             Get Started
           </button>
